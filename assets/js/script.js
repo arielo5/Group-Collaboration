fa98d4c1b4aae2dcab11b4173869732feddd9775
Scott T <scott.tessm@gmail.com>
2021-05-26 15:46:14 -0500

    Added more js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
+let cuisineDropdown;
 let ingredientString;
+let orderedListForRecipe = document.querySelector("#forRecipe");
+let randomRecipeRequest;
+let recipeTextArea;
 let stepDetails;
 let stepIngredients;
 let steps;
 let thingsToMake;
+let userSelectedCuisine;
 
 
 // Fetches the data for a specific recipe
-function testFunction () {
+function getRecipeDetails () {
     fetch('https://api.spoonacular.com/recipes/324694/analyzedInstructions?apiKey=6bcf2249e71b4f518c9bc66ffb045b87')
         .then(function (response) {
         return response.json();
@@ -16,15 +21,41 @@ function testFunction () {
         console.log(data[0].steps);
         thingsToMake = data.length
         steps = data[0].steps;
-        goThroughSteps();
+        goThroughRecipeSteps();
     });
 }
 
+// Takes in the user specified search criteria and uses it to find them a random recipe matching that criteria
+function getRandomRecipe (recipeRequestLink) {
+    fetch(recipeRequestLink)
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (data) {
+        console.log(data);
+    });
+}
+
+// Gets the user selected cuisine from the cuisine dropdown
+function getCuisineSelection () {
+    userSelectedCuisine = cuisineDropdown.value;
+    if (userSelectedCuisine === "Default"){
+        randomRecipeRequest = 'https://api.spoonacular.com/recipes/random?number=1&apiKey=6bcf2249e71b4f518c9bc66ffb045b87';
+    } else {
+        randomRecipeRequest = 'https://api.spoonacular.com/recipes/random?number=1&tags=' + userSelectedCuisine + '&apiKey=6bcf2249e71b4f518c9bc66ffb045b87';
+    }
+    getRandomRecipe(randomRecipeRequest);
+}
+
 // Goes through each of the steps for a recipe
-function goThroughSteps () {
+function goThroughRecipeSteps () {
     for (let i = 0; i < steps.length; i++) {
         stepDetails = steps[i].step;
-        if (stepDetails[i].ingredients !== null) {
+        let listItem = document.createElement("LI");
+        listItem.innerHTML = stepDetails;
+        orderedListForRecipe.appendChild(listItem);
+        console.log(stepDetails);
+        if (steps[i].ingredients.length !== 0) {
             stepIngredients = steps[i].ingredients;
             parseIngredientsIntoString();
         }
@@ -43,8 +74,11 @@ function parseIngredientsIntoString () {
                 ingredientString = ingredientString.concat(', ', ingredient);
             }
         }
+        let postedIngredientString = document.createElement("P");
+        postedIngredientString.innerHTML = '- Ingredients Needed: ' + ingredientString;
+        orderedListForRecipe.appendChild(postedIngredientString);
         console.log(ingredientString);
     }
 }
 
-testFunction();
+//getRecipeDetails();
