9cd33089dcf6fdc95ae1a911ede2587a75c66083
Scott T <scott.tessm@gmail.com>
2021-05-30 22:10:41 -0500

    Added functionality to past search areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,9 @@ let listOfIngredients;
 let movieOnlyButton = document.querySelector("#movieShBtn");
 let orderedListForRecipe = document.querySelector("#orderedListForRecipe");
 let pastCuisineArea = document.querySelector("#pastCuisineHistory");
+let pastMovieArea = document.querySelector("#pastMovieHistory");
 let pickBothButton = document.querySelector("#bothShBtn");
+let querySpecificMovieLink;
 let randomRecipeRequest;
 let recipeIdentifier;
 let recipeImageLink;
@@ -43,6 +45,31 @@ let queryString;
         ajaxMovieCall(queryURL);
     }
     
+// Creates a URL to be queried based off of past movie search history
+function createSpecificMovieURL (movieID) {
+    movieID = movieID.toString();
+    querySpecificMovieLink = 'http://api.themoviedb.org/3/movie/' + movieID + '?api_key=' + api_key;
+    console.log(querySpecificMovieLink);
+    specificMovieCall(querySpecificMovieLink);
+}
+
+//Query for a specific movie
+function specificMovieCall (queryURL) {
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (response) {
+            console.log(response);
+        },
+    }).then(function (response) {
+        console.log(response);
+        renderMovie(response);
+    });
+}
+
 // call function
 function ajaxMovieCall(queryURL) {
     $.ajax({
@@ -63,6 +90,7 @@ function ajaxMovieCall(queryURL) {
 //fuction that renders movie
 function renderMovie(movie) {
     let posterImageCode = movie.backdrop_path;
+    let movieIdentifier = movie.id;
     let posterImageURL = `https://image.tmdb.org/t/p/w500/${posterImageCode}`;
     document.getElementById("picturelink").src = posterImageURL;
 
@@ -76,6 +104,7 @@ function renderMovie(movie) {
     document.getElementById("ratings").innerHTML = "Rating: " + ratingMovie;
     let moviesList = $('#movies-list');
     movieImgPlaceholder.setAttribute("class", "image is-5by3");
+    addMovieToHistory(movieIdentifier, titleMovie, posterImageCode);
 }
 
 function cleanMoviesList(movie) {
@@ -89,6 +118,7 @@ function getRandomMovie(movies) {
 
     return movie;
 }
+
 //for loop if user would like to display more than one movie
 function renderMovies(movies, length) {
     cleanMoviesList();
@@ -98,6 +128,7 @@ function renderMovies(movies, length) {
         renderMovie(randomMovie);
     }
 }
+
 
 // Fetches the data for a specific recipe
 function getRecipeDetails(recipeIdentifier) {
@@ -261,21 +292,39 @@ function createRecipeButtons () {
     if(localStorage.getItem("PreviousRecipes")!== null) {
         let recentRecipesList = JSON.parse(localStorage.getItem("PreviousRecipes"));
         for (let i = 0; i < recentRecipesList.items.length; i++){
-            let newButton = document.createElement("BUTTON");
-            newButton.setAttribute("class", "button is-large is-fullwidth recentCuisine");
-            newButton.setAttribute("data-id", recentRecipesList.items[i].id);
-            pastCuisineArea.appendChild(newButton);
+            let newDiv = document.createElement("DIV");
+            newDiv.setAttribute("class", "box");
+            newDiv.setAttribute("data-id", recentRecipesList.items[i].id);
+            pastCuisineArea.appendChild(newDiv);
+            let newArticle = document.createElement("ARTICLE");
+            newArticle.setAttribute("data-id", recentRecipesList.items[i].id);
+            newArticle.setAttribute("class", "media");
+            newDiv.appendChild(newArticle);
+            let anotherDiv = document.createElement("DIV");
+            anotherDiv.setAttribute("data-id", recentRecipesList.items[i].id);
+            anotherDiv.setAttribute("class", "media-left");
+            newArticle.appendChild(anotherDiv);
             let newFigure = document.createElement("FIGURE");
-            newFigure.setAttribute("class", "image is-48x48 ml-0");
-            newButton.appendChild(newFigure);
+            newFigure.setAttribute("data-id", recentRecipesList.items[i].id);
+            newFigure.setAttribute("class", "image is-64x64");
+            anotherDiv.appendChild(newFigure);
             let newImg = document.createElement("IMG");
+            newImg.setAttribute("data-id", recentRecipesList.items[i].id);
             newImg.setAttribute("src", recentRecipesList.items[i].imgLink);
             newImg.setAttribute("alt", "Cuisine");
             newFigure.appendChild(newImg);
-            let newSpan = document.createElement("SPAN");
-            newSpan.setAttribute("class", "is-size-4 has-text-weight-bold pl-6");
-            newSpan.innerHTML = recentRecipesList.items[i].title;
-            newButton.appendChild(newSpan);
+            let yetAnotherDiv = document.createElement("DIV");
+            yetAnotherDiv.setAttribute("data-id", recentRecipesList.items[i].id);
+            yetAnotherDiv.setAttribute("class", "media-content");
+            newArticle.appendChild(yetAnotherDiv);
+            let fourthDiv = document.createElement("DIV");
+            fourthDiv.setAttribute("data-id", recentRecipesList.items[i].id);
+            fourthDiv.setAttribute("class", "content has-text-weight-bold is-size-4 has-text-centered");
+            yetAnotherDiv.appendChild(fourthDiv);
+            let newParagraph = document.createElement("P");
+            newParagraph.setAttribute("data-id", recentRecipesList.items[i].id);
+            newParagraph.innerHTML = recentRecipesList.items[i].title;
+            fourthDiv.appendChild(newParagraph);
         }
     }
 }
@@ -284,7 +333,7 @@ function createRecipeButtons () {
 function handlePastRecipeAreaClick (event) {
     event.preventDefault();
     let newRecipe = event.target;
-    if (newRecipe.nodeName === "BUTTON" || newRecipe.nodeName === "FIGURE" || newRecipe.nodeName === "IMG" || newRecipe.nodeName === "SPAN"){
+    if (newRecipe.nodeName === "DIV" || newRecipe.nodeName === "FIGURE" || newRecipe.nodeName === "IMG" || newRecipe.nodeName === "ARTICLE" || newRecipe.nodeName === "P"){
         newRecipe = newRecipe.getAttribute("data-id");
         let recentRecipesList = JSON.parse(localStorage.getItem("PreviousRecipes"));
         for (let i = 0; i < recentRecipesList.items.length; i++){
@@ -302,11 +351,112 @@ function handlePastRecipeAreaClick (event) {
 
 }
 
+// When a movie is searched it is then added to the local storage
+function addMovieToHistory (movieID, movieTitle, moviePoster) {
+    if (localStorage.getItem('PreviousMovies') === null){
+        let movieArray = [];
+        let movieObject = {};
+        let item = {
+            "id": movieID,
+            "title": movieTitle,
+            "poster": moviePoster
+        }
+        movieObject.items = movieArray;
+        movieObject.items.unshift(item);
+        localStorage.setItem('PreviousMovies', JSON.stringify(movieObject));
+    }else{
+        movieObject = JSON.parse(localStorage.getItem("PreviousMovies"));
+        for (let i = 0; i < movieObject.items.length; i++){
+            if (movieObject.items[i].id === movieID) {
+                movieObject.items.splice(i,1);
+            }
+        }
+        if(movieObject.items.length > 2) {
+            movieObject.items.pop();
+        }
+        let item = {
+            "id": movieID,
+            "title": movieTitle,
+            "poster": moviePoster
+        }
+        movieObject.items.unshift(item);
+        localStorage.setItem('PreviousMovies', JSON.stringify(movieObject));
+    }
+    createMovieButtons();
+}
+
+// Creates the buttons on the page for the Recent Movies Area
+function createMovieButtons () {
+    while (pastMovieArea.firstChild){
+        pastMovieArea.removeChild(pastMovieArea.firstChild);
+    }
+    if(localStorage.getItem("PreviousMovies")!== null) {
+        let recentMoviesList = JSON.parse(localStorage.getItem("PreviousMovies"));
+        for (let i = 0; i < recentMoviesList.items.length; i++){
+            let newDiv = document.createElement("DIV");
+            newDiv.setAttribute("class", "box");
+            newDiv.setAttribute("data-id", recentMoviesList.items[i].id);
+            pastMovieArea.appendChild(newDiv);
+            let newArticle = document.createElement("ARTICLE");
+            newArticle.setAttribute("class", "media");
+            newArticle.setAttribute("data-id", recentMoviesList.items[i].id);
+            newDiv.appendChild(newArticle);
+            let anotherDiv = document.createElement("DIV");
+            anotherDiv.setAttribute("class", "media-left");
+            anotherDiv.setAttribute("data-id", recentMoviesList.items[i].id);
+            newArticle.appendChild(anotherDiv);
+            let newFigure = document.createElement("FIGURE");
+            newFigure.setAttribute("class", "image is-64x64");
+            newFigure.setAttribute("data-id", recentMoviesList.items[i].id);
+            anotherDiv.appendChild(newFigure);
+            let newImg = document.createElement("IMG");
+            newImg.setAttribute("src", 'https://image.tmdb.org/t/p/w500/' + recentMoviesList.items[i].poster);
+            newImg.setAttribute("data-id", recentMoviesList.items[i].id);
+            newImg.setAttribute("alt", "Cuisine");
+            newFigure.appendChild(newImg);
+            let yetAnotherDiv = document.createElement("DIV");
+            yetAnotherDiv.setAttribute("data-id", recentMoviesList.items[i].id);
+            yetAnotherDiv.setAttribute("class", "media-content");
+            newArticle.appendChild(yetAnotherDiv);
+            let fourthDiv = document.createElement("DIV");
+            fourthDiv.setAttribute("data-id", recentMoviesList.items[i].id);
+            fourthDiv.setAttribute("class", "content has-text-weight-bold is-size-4 has-text-centered");
+            yetAnotherDiv.appendChild(fourthDiv);
+            let newParagraph = document.createElement("P");
+            newParagraph.setAttribute("data-id", recentMoviesList.items[i].id);
+            newParagraph.innerHTML = recentMoviesList.items[i].title;
+            fourthDiv.appendChild(newParagraph);
+        }
+    }
+}
+
+// Handles when a button is clicked in the Movie History Area
+function handlePastMovieAreaClick (event) {
+    event.preventDefault();
+    let newMovie = event.target;
+    if (newMovie.nodeName === "DIV" || newMovie.nodeName === "FIGURE" || newMovie.nodeName === "IMG" || newMovie.nodeName === "ARTICLE" || newMovie.nodeName === "P"){
+        newMovie = newMovie.getAttribute("data-id");
+        console.log(newMovie);
+        let recentMoviesList = JSON.parse(localStorage.getItem("PreviousMovies"));
+        for (let i = 0; i < recentMoviesList.items.length; i++){
+            if (recentMoviesList.items[i].id == newMovie) {
+                movieID = recentMoviesList.items[i].id;
+                createSpecificMovieURL(movieID);
+            }
+    }
+    } else{
+        return;
+    }
+
+}
+
 cuisineDropdown.addEventListener("click", setUserCuisineChoice);
 cuisineOnlyButton.addEventListener("click", getCuisineSelection);
 movieDropdown.addEventListener("click", setUserMovieChoice);
 movieOnlyButton.addEventListener("click", createMovieQueryURL);
 pickBothButton.addEventListener("click", pickBothHandle);
 pastCuisineArea.addEventListener("click", handlePastRecipeAreaClick);
+pastMovieArea.addEventListener("click", handlePastMovieAreaClick);
 
 createRecipeButtons();
+createMovieButtons();
